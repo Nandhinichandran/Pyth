85a6b1c70e41ab89f7d3dde9fb5bcba4de650b7d
Nandhini <32250902+Nandhinichandran@users.noreply.github.com>
2018-01-26 10:21:29 +0530

    Create pri2.py

---
+++ pri2.py
@@ -0,0 +1,10 @@
+x=int(input())
+y=int(input())
+for n in range(x,y+1):
+    if(n>1):
+        for i in range(2,n):
+            if(n%i==0):
+                break
+        else:
+                print(n)
+            
